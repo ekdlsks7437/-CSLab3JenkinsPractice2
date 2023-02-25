add9b5c12fffa74cfc421c2c1c3142ac301d3551
ekdlsks7437 <ekdlsks7437@gmail.com>
2023-02-24 20:38:13 -0700

    finished writing functions

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,4 @@
-
+import java.util.UUID;
 class Calculator {
 
     Calculator(){
@@ -39,7 +39,13 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacciNumberFinder(n-1) + fibonacciNumberFinder(n-2);
+        }
     }
 
 
@@ -51,7 +57,15 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        if (n == 0) {
+            return "0";
+        }
+        String binary = "";
+        while (n > 0) {
+            binary = (n % 2) + binary;
+            n /= 2;
+        }
+        return binary;
     }
 
     /*
@@ -63,8 +77,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        String randomString = UUID.randomUUID().toString().replace("-", "");
+        return n + randomString;
     }
 
 
